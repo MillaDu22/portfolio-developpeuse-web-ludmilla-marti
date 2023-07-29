7df2a92eff75da688e76d24d341812143cc71be8
Milla22 <marti.ludmilla@orange.fr>
2023-07-30 00:21:33 +0200

    homepage et cv responsive, ajout d'un lien acuueil dans le header

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,19 +3,17 @@ import {Link} from "react-router-dom";
 
 function Card  ({id, cover, title, informations}){
     return (
-        <main>
+        <div className ="card-box">
             <Link className= "link-card" to={ `/projet/${id}`}>
                 <div className = "card">
                     <img src= {cover} alt = "coverCard" className="coverCard"/>
                     <div className="card-content">
-                        <h3 className = "titreCard">{ title }</h3>
-                        <span className="back-info">
                         <p className ="informations">{ informations }</p>
-                        </span>
+                        <h3 className = "titreCard">{ title }</h3>
                     </div>
                 </div>
             </Link>
-        </main>
+        </div>
     )
 }
 export default Card;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,11 +8,14 @@ function Header() {
         <header className="header">
         <Link className="header_link" to="/">Ludmilla Marti - Développeuse Web</Link>
             <nav className="nav">
-                <Link className="nav_item" to="/a-propos-page">
-                    <p className="nav_item_text">A propos</p>
+                <Link className="nav_item1" to="/home">
+                    <p className="nav_item_text1">Accueil</p>
                 </Link>
-                <Link className="nav_item" to="/cvpage">
-                    <p className="nav_item_text">Cv</p>
+                <Link className="nav_item2" to="/a-propos-page">
+                    <p className="nav_item_text2">A propos de moi</p>
+                </Link>
+                <Link className="nav_item3" to="/cvpage">
+                    <p className="nav_item_text3">Cv</p>
                 </Link>
             </nav>
         </header>
